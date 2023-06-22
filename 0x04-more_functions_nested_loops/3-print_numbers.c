57e746733bc800d6f1b76757547daa2dfbd00fd0
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-22 17:01:19 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
 
 void print_numbers(void)
 {
-int i = 0;
+	int i = 0;
 
-while (i <= 9)
-{
-_putchar(0 + i);
-i++;
-}
-_putchar(n);
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
 }
