38f24df9c27992f126ced8f78b0a2c9330241e75
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-22 19:12:21 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,17 +7,17 @@
 
 void print_square(int size)
 {
-int width, height;
+	int width, height;
 
-if (size <= 0)
-_putchar(n);
-else
-{
-for (height = 0; height < size; height++)
-{
-for (width = 0; width < size; width++)
-_putchar(#);
-_putchar(n);
-}
-}
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
 }
