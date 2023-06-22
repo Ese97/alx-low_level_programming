acba55776eab21ac8dd0385a9441dee55b38f41d
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-22 19:07:20 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,20 +7,20 @@
 
 void print_diagonal(int n)
 {
-int column, space, i;
+	int column, space, i;
 
-if (n <= 0)
-_putchar(n);
-else
-{
-for (column = 0; column < n; column++)
-{
-i = column - 1;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (column = 0; column < n; column++)
+		{
+			i = column - 1;
 
-for (space = 0; space <= i; space++)
-_putchar( );
-_putchar(\);
-_putchar(n);
-}
-}
+			for (space = 0; space <= i; space++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
