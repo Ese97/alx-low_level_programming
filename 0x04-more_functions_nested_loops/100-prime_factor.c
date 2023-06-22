7e5bf2d4607bc7b8b21171f68f8ee39aeeaeb83a
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-22 19:33:17 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-unsigned long num = 612852475143;
-unsigned long i;
-unsigned long large_prime;
+	unsigned long num = 612852475143;
+	unsigned long i;
+	unsigned long large_prime;
 
-for (i = 2; i <= num; i++)
-{
-if (num % i == 0)
-{
-large_prime = i;
-num /= large_prime;
-}
-}
-printf("%lu\n", large_prime);
-return (0);
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			large_prime = i;
+			num /= large_prime;
+		}
+	}
+	printf("%lu\n", large_prime);
+	return (0);
 }
