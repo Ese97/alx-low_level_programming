f654c35a2c630778c13d6a8240c317fe6b14c253
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-22 19:26:52 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,27 +5,27 @@
  */
 void print_triangle(int size)
 {
-int i;
-int j;
-int k;
+	int i;
+	int j;
+	int k;
 
-if (size <= 0)
-{
-_putchar(n);
-} else
-{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
 
-for (i = 1; i <= size; i++)
-{
-for (j = size - i ; j > 0; j--)
-{
-_putchar( );
-}
-for (k = 0; k < i; k++)
-{
-_putchar(#);
-}
-_putchar(n);
-}
-}
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
