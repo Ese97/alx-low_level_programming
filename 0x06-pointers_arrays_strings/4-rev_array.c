8ddff5eb3ea3255f68f2a0eddf809cf8c2bc314c
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-29 13:30:48 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,14 @@
  */
 void reverse_array(int *a, int n)
 {
-int temp;
-int fd = 0;
-int rd;
+	int temp;
+	int fd = 0;
+	int rd;
 
-for (fd = 0, rd = n - 1; fd < rd; fd++, rd--)
-{
-temp = a[fd];
-a[fd] = a[rd];
-a[rd] = temp;
-}
+	for (fd = 0, rd = n - 1; fd < rd; fd++, rd--)
+	{
+		temp = a[fd];
+		a[fd] = a[rd];
+		a[rd] = temp;
+	}
 }
