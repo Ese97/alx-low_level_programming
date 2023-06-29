faeba54b6c58489d1ebc74e55e4cac6b0cf361a6
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-06-29 13:15:02 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,22 +11,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = 0;
-int i = 0;
+	int dest_len = 0;
+	int i = 0;
 
-/* calculate the length of dest */
-while (dest[dest_len] != 0)
-{
-dest_len++;
-}
-/* copy up to n characters from src to dest */
-while (i < n && src[i] != 0)
-{
-dest[dest_len + i] = src[i];
-i++;
-}
-/* Add null terminator to the end of the concatenated string */
-dest[dest_len + i] = 0;
+	/* calculate the length of dest */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	/* copy up to n characters from src to dest */
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	/* Add null terminator to the end of the concatenated string */
+	dest[dest_len + i] = '\0';
 
-return (dest);
+	return (dest);
 }
