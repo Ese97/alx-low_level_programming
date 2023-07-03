729a7f0d4fe30c88ac3a543cbb3959bc1ceca2db
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-07-03 17:21:51 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,25 +10,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-if (!*needle)
-{
-return (haystack);
-}
-while (*haystack)
-{
-char *h = haystack;
-char *n = needle;
+	if (!*needle)
+	{
+		return (haystack);
+	}
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
 
-while (*h && *n && *h  == *n)
-{
-h++;
-n++;
-}
-if (!*n)
-{
-return (haystack);
-}
-haystack++;
-}
-return (0);
+		while (*h && *n && *h  == *n)
+		{
+			h++;
+			n++;
+		}
+		if (!*n)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
 }
