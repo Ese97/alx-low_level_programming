7a9ce1a0ab77583b8efcf72efd74672da81c9e2f
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-07-05 14:29:50 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s == 0)
-return (0);
-else
-return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -23,13 +23,13 @@ return (1 + _strlen_recursion(s + 1));
  */
 int comparator(char *s, int n1, int n2)
 {
-if (*(s + n1) == *(s + n2))
-{
-if (n1 == n2 || n1 == n2 + 1)
-return (1);
-return (0 + comparator(s, n1 + 1, n2 - 1));
-}
-return (0);
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
 }
 
 /**
@@ -39,7 +39,7 @@ return (0);
  */
 int is_palindrome(char *s)
 {
-if (*s == 0)
-return (1);
-return (comparator(s, 0, _strlen_recursion(s) - 1));
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
