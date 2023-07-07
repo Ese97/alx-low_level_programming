ff9acda2d57b6b39b1fdd469c5d0eb2138b75b4c
Doris Kator <111313704+Ese97@users.noreply.github.com>
2023-07-07 22:53:37 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-(void) argc;
+	(void) argc;
 
-if (argv[1] && argv[2])
-{
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-return (0);
-}
-else
-printf("Error\n");
-return (1);
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
 }
